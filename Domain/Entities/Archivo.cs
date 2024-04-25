8ac321e7431567c54b27a4f61a821b556120a129
Gaudencio Jurado <gjurado@virtualidevs.com>
2024-04-24 18:33:28 -0600

    Add project files.

---
+++ Domain/Entities/Archivo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Archivo
+    {
+        public int Id { get; set; }
+        public int FacturaId { get; set; }
+        public Factura Factura { get; set; }
+        public string Nombre { get; set; }
+        public long Tamano { get; set; }
+        public string Extension { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string Ruta { get; set; }
+    }
+}
---
+++ Domain/Entities/Proveedor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public string RFC { get; set; }
+        public string Direccion { get; set; }
+        public bool Activo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+        public List<Factura> Facturas { get; set; } = new List<Factura>();
+    }
+}
