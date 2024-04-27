7f0cc539a7c59f343c0da9960640f56bbf3a1109
Gaudencio Jurado <gjurado@virtualidevs.com>
2024-04-26 19:46:13 -0600

    added serilog to centralize logs

--- CustomSoftWebApi/Program.cs
+++ CustomSoftWebApi/Program.cs
@@ -7,50 +7,69 @@ using Infrastructure.Repositories.Core;
 using Common;
 using Serilog;
 
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up!");
+
 try
 {
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSerilog((services, lc) => lc
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console());
+
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddScoped<IDatabaseService, DatabaseService>(provider => new DatabaseService(connectionString));
+    builder.Services.AddScoped<IProveedorRepository, ProveedorRepository>();
+
+    // register mapper profile
+    builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Application.Handlers.QueryHandlers.GetProveedorListHandler).Assembly));
+
+    builder.Services.AddControllers();
+
+    builder.Services
+        .AddDefaultApiKeyGenerator(new ApiKeyGenerationOptions
+        {
+            KeyPrefix = "KP-",
+            GenerateUrlSafeKeys = true,
+            LengthOfKey = 32
+        })
+        .AddDefaultClaimsPrincipalFactory()
+        .AddApiKeys()
+        .AddSingleton<IClientsService, InMemoryClientsService>()
+        .AddMemoryCache()
+        .AddSingleton<IApiKeysCacheService, CacheService>();
+
+    await using var app = builder.Build();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    await app.RunAsync();
+
+    Log.Information("Api stopped");
+    return 0;
 
 }
-catch (Exception)
+catch (Exception ex)
 {
-
-    throw;
+    Log.Fatal(ex, "An unhandled exception occurred during bootstrapping");
+    return 1;
 }
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddScoped<IDatabaseService, DatabaseService>(provider => new DatabaseService(connectionString));
-builder.Services.AddScoped<IProveedorRepository, ProveedorRepository>();
-
-// register mapper profile
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Application.Handlers.QueryHandlers.GetProveedorListHandler).Assembly));
-
-builder.Services.AddControllers();
-
-builder.Services
-    .AddDefaultApiKeyGenerator(new ApiKeyGenerationOptions
-    {
-        KeyPrefix = "KP-",
-        GenerateUrlSafeKeys = true,
-        LengthOfKey = 32
-    })
-    .AddDefaultClaimsPrincipalFactory()
-    .AddApiKeys()
-    .AddSingleton<IClientsService, InMemoryClientsService>()
-    .AddMemoryCache()
-    .AddSingleton<IApiKeysCacheService, CacheService>();
-
-var app = builder.Build();
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
